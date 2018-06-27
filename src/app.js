3ea4403357788ba8ec58708476a8d6933167598e
Lakshay Makkar <lakshay@mitter.io>
2018-06-27 16:56:23 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { createStore } from 'redux'
+import fetchApp from './reducers'
+import { fetchData } from './actions'
+import {bindActionCreators} from 'redux'
+
+const store = createStore(fetchApp)
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    componentDidMount(){
+      this.props.fetchData()
+    }
+    render()
+  }
+}
+
+function mapDisptchToProps(dispatch) {
+  return bindActionCreators({fetchData},dispatch)
+}
+
+export default connect(null,mapDisptchToProps)(App)
